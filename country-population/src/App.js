b8391b0646e09044fa09a4f0c1f54da432b911bf
Bharadwaj Kuna <bkuna@mandmdirect.com>
2023-12-10 17:32:51 +0000

    NavBar is completed

--- country-population/src/App.js
+++ country-population/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      Country Population
+      <NavBar />
     </div>
   );
 }
